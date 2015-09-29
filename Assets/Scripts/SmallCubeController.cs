c964d9f4b0f23e6f7054e330b236cf4916efc76d
limered <me@limered.de>
2015-09-29 13:56:13 +0200

    color change of small cubes

--- Assets/Scripts/SmallCubeController.cs
+++ Assets/Scripts/SmallCubeController.cs
@@ -1,36 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
-using Assets.Scripts.Physics;
-using Assets.Scripts.Collisions;
 
 namespace Assets.Scripts
 {
     internal class SmallCubeController : ObjectController
     {
-        Color baseCol = new Color(0.833f, 0.872f, 1);
-        Color currentColor;
-        float startTime = -20f;
-        float maxTime = 10f; //sec
-        float maxTimeInv = 1f / 10f;
+        private Color baseCol = new Color(0.833f, 0.872f, 1);
+        private Color red = new Color(1, 0, 0);
+        private Color currentColor;
+        private float startTime = -20f;
+        private float maxTime = 10f; //sec
+        private float maxTimeInv = 1f / 10f;
 
-        void Update() {
+        private bool _lastAwakeState;
+
+        private void Update()
+        {
             var render = GetComponent<MeshRenderer>();
-            if (!isAwake) render.material.color = baseCol;//Time.realtimeSinceStartup - startTime > maxTime)
-            else
+            if (!isAwake)
             {
-                //var time = Time.realtimeSinceStartup - startTime;
-                //var percent = time * maxTimeInv;
+                if (_lastAwakeState != isAwake)
+                {
+                    startTime = Time.realtimeSinceStartup;
+                }
+                var time = Time.realtimeSinceStartup - startTime;
+                var percent = time * maxTimeInv;
 
-                //var diff = baseCol - currentColor;
-                //diff = diff * percent;
+                var diff = baseCol - currentColor;
+                diff = diff * percent;
 
-                //currentColor += diff;
+                currentColor += diff;
 
                 render.material.color = currentColor;
             }
+            else
+            {
+                if (_lastAwakeState != isAwake)
+                {
+                    currentColor = red;
+                }
+                render.material.color = currentColor;
+            }
+
+            _lastAwakeState = isAwake;
 
             if (!isAwake) return;
-            
+
             Vector3 force = Vector3.zero;
             LinearForces(ref force);
 
@@ -47,8 +61,8 @@ namespace Assets.Scripts
 
         public void ChangeColor(float time)
         {
-            startTime = time;
-            currentColor = new Color(1, 0, 0);
+            //startTime = time;
+            //currentColor = red;
         }
     }
 }
